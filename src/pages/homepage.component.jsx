217978c26152ed67e920e64f84a6c77747801813
Adithya Nair <adityasnair2000@gmail.com>
2022-03-15 21:29:31 +0530

    placed the directory and the menu items into their components an d nnnd sty

--- src/pages/homepage.component.jsx
+++ src/pages/homepage.component.jsx
@@ -1,42 +1,11 @@
 import React from 'react';
 import './homepage.styles.css';
-
+import { DirectoryMenu } from '../components/directory-menu/directory-menu.component';
 
 export const HomePage = () => {
     return(
         <div className='home-page'>
-            <div className='directory-menu'>
-                <div className='menu-item'>
-                    <div className='content'>
-                        <h1 className='title'>HATS</h1>
-                        <span className='sub-title'>SHOP NOW</span>
-                    </div>
-                </div>
-                <div className='menu-item'>
-                    <div className='content'>
-                        <h1 className='title'>JACKETS</h1>
-                        <span className='sub-title'>SHOP NOW</span>
-                    </div>
-                </div>
-                <div className='menu-item'>
-                    <div className='content'>
-                        <h1 className='title'>SNEAKERS</h1>
-                        <span className='sub-title'>SHOP NOW</span>
-                    </div>
-                </div>
-                <div className='menu-item'>
-                    <div className='content'>
-                        <h1 className='title'>MENS</h1>
-                        <span className='sub-title'>SHOP NOW</span>
-                    </div>
-                </div>
-                <div className='menu-item'>
-                    <div className='content'>
-                        <h1 className='title'>WOMENS</h1>
-                        <span className='sub-title'>SHOP NOW</span>
-                    </div>
-                </div>
-            </div>
+            <DirectoryMenu />
         </div>
     );
 }
